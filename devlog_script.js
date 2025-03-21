e8958fc1da3ee5d19b560ace7eec43b079e86a03
Hans Hofner <hhofner@gmail.com>
2025-03-21 14:51:28 +0900

    update devlog script and entry

--- devlog_script.js
+++ devlog_script.js
@@ -1,89 +1,117 @@
 /* Configuration */
 
-var REPO = 'slims.hhofner.com';
-var SOURCE = 'src/content/devlog';
-var SITE = 'blog.hhofner.com';
-var PATH = '';
-var OWNER = 'hhofner';
+var REPO = "slims.hhofner.com";
+var SOURCE = "src/content/devlog";
+var SITE = "blog.hhofner.com";
+var PATH = "";
+var OWNER = "hhofner";
 
-var BASE = 'https://api.github.com/repos';
+var BASE = "https://api.github.com/repos";
 
 /* Connection */
 
 var http = HTTP.create();
-var credential = Credential.create('Github', 'Insert Github API token from github.com/settings/tokens');
-credential.addTextField('token', 'Personal access token');
+var credential = Credential.create(
+	"Github",
+	"Insert Github API token from github.com/settings/tokens",
+);
+credential.addTextField("token", "Personal access token");
 credential.authorize();
 
-var TOKEN = credential.getValue('token');
+var TOKEN = credential.getValue("token");
 
 /* Utils */
 
 function pad(i) {
-  return (i < 10 ? '0' : '') + i;
+	return (i < 10 ? "0" : "") + i;
 }
 
 function formatDate(date) {
-  return date.toISOString().split('T')[0];
+	return date.toISOString().split("T")[0];
 }
 
 function getSlug(content) {
-  var slug = content.match(/^slug: (.*)$/m);
-  if (slug) return slug[1];
-  var firstLine = content.split('\n')[0].trim();
-  if (!firstLine) throw new Error('No valid title found');
-  return firstLine.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/^-|-$/g, '');
+	var slug = content.match(/^slug: (.*)$/m);
+	if (slug) return slug[1];
+	var firstLine = content.split("\n")[0].trim();
+	if (!firstLine) throw new Error("No valid title found");
+	return firstLine.toLowerCase().replace(/[^a-z0-9]+/g, "-").replace(
+		/^-|-$/g,
+		"",
+	);
 }
 
 function createFrontMatter(content, publishDate) {
-  return `---\ncontent: \"${content.split('\n')[0]}\"\ndate: ${formatDate(publishDate)}\ndraft: true\n---\n\n`;
+	return `---\ncontent: \"${content.split("\n")[0]}\"\ndate: ${
+		formatDate(publishDate)
+	}\ndraft: false\n---\n\n`;
 }
 
 function req(method, url, data) {
-  console.log(method + ' ' + url);
-  var res = http.request({
-	method: method,
-	url: url,
-	headers: {
-	  'Accept': 'application/vnd.github.v3+json',
-	  'Authorization': 'token ' + TOKEN
-	},
-	encoding: 'json',
-	data: data
-  });
-  if (res.responseText) res.data = JSON.parse(res.responseText);
-  return res;
+	console.log(method + " " + url);
+	var res = http.request({
+		method: method,
+		url: url,
+		headers: {
+			"Accept": "application/vnd.github.v3+json",
+			"Authorization": "token " + TOKEN,
+		},
+		encoding: "json",
+		data: data,
+	});
+	if (res.responseText) res.data = JSON.parse(res.responseText);
+	return res;
 }
 
 function urlformat(/* ...bits */) {
-  var d = new Date();
-  var bits = [].filter.call(arguments, function (x) { return !!x; });
-  return bits.join('/').replace('{year}', d.getFullYear()).replace('{month}', pad(d.getMonth() + 1)).replace('{day}', pad(d.getDate()));
+	var d = new Date();
+	var bits = [].filter.call(arguments, function (x) {
+		return !!x;
+	});
+	return bits.join("/").replace("{year}", d.getFullYear()).replace(
+		"{month}",
+		pad(d.getMonth() + 1),
+	).replace("{day}", pad(d.getDate()));
 }
 
 /* Logic */
 
 function main() {
-  var content = draft.content;
-  var slug = getSlug(content);
-  var publishDate = new Date();
-  content = createFrontMatter(content, publishDate) + content;
+	var content = draft.content;
+	var slug = getSlug(content);
+	var publishDate = new Date();
+	content = createFrontMatter(content, publishDate) + content;
 
-  var url = urlformat(BASE, OWNER, REPO, 'contents', SOURCE, PATH, `${formatDate(publishDate)}-${slug}.md`);
-  
-  var b64 = Base64.encode(content);
-  var res = req('GET', url, null);
+	var url = urlformat(
+		BASE,
+		OWNER,
+		REPO,
+		"contents",
+		SOURCE,
+		PATH,
+		`${formatDate(publishDate)}-${slug}.md`,
+	);
 
-  if ((res.statusCode == 200) && (res.data.type != 'file')) {
-	throw 'Target file exists, but is not a file: ' + res.data.type;
-  } else if (res.statusCode == 200) {
-	res = req('PUT', url, { message: 'Update via Drafts: ' + slug, content: b64, sha: res.data.sha });
-  } else {
-	res = req('PUT', url, { message: 'New via Drafts: ' + slug, content: b64 });
-  }
+	var b64 = Base64.encode(content);
+	var res = req("GET", url, null);
 
-  var result = urlformat(SITE, PATH, slug, '');
-  app.setClipboard(result);
+	if ((res.statusCode == 200) && (res.data.type != "file")) {
+		throw "Target file exists, but is not a file: " + res.data.type;
+	} else if (res.statusCode == 200) {
+		res = req("PUT", url, {
+			message: "Update via Drafts: " + slug,
+			content: b64,
+			sha: res.data.sha,
+		});
+	} else {
+		res = req("PUT", url, {
+			message: "New via Drafts: " + slug,
+			content: b64,
+		});
+	}
+
+	var result = urlformat(SITE, PATH, slug, "");
+	app.setClipboard(result);
 }
 
 main();
